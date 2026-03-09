ad9225e46cc25db45e964b2c803c2f8883ad1321
askhmza-prime <askhmza@gmail.com>
2026-03-09 13:42:03 +0530

    Add StarsBackground component for animated stars

--- src/components/StarsBackground.jsx
+++ src/components/StarsBackground.jsx
@@ -1,1 +1,31 @@
+const StarsBackground = () => {
+  const stars = Array.from({ length: 60 }, (_, i) => ({
+    id: i,
+    size: Math.random() * 3 + 1,
+    top: Math.random() * 100,
+    left: Math.random() * 100,
+    duration: Math.random() * 3 + 2,
+    delay: Math.random() * 3,
+  }))
 
+  return (
+    <div className="stars-bg">
+      {stars.map(star => (
+        <div
+          key={star.id}
+          className="star"
+          style={{
+            width: star.size,
+            height: star.size,
+            top: `${star.top}%`,
+            left: `${star.left}%`,
+            '--duration': `${star.duration}s`,
+            animationDelay: `${star.delay}s`,
+          }}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default StarsBackground
